fd71a35fe1bf5581c5beb44903192c8926126a5a
fir3e <fir3ez@gmail.com>
2023-09-08 15:37:57 +0200

    working djangoblog first commit

---
+++ blog/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+from .models import Comment, Post
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        exclude = ("status", "created on", "updated_on")
+
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "content": forms.Textarea(attrs={"class": "form-control content2"}),
+            "image": forms.FileInput(attrs={"class": "form-control"}),
+            "author": forms.HiddenInput(),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = {
+            "post",
+            "author",
+            "text",
+        }
+
+        widgets = {
+            "post": forms.HiddenInput(),
+            "author": forms.HiddenInput(),
+            "text": forms.Textarea(attrs={"class": "form-control content2"}),
+        }
